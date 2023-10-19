9f9735360370724eb31eaa5433a75e9f3d07c5e6
Alejandro Cores <91358116+alec0322@users.noreply.github.com>
2023-10-19 16:28:08 -0400

    Update itinero_trainer.py
    Modularized some of the steps to make the CrimeClassifier easier to deploy/test

--- backend/app/itinero_trainer.py
+++ backend/app/itinero_trainer.py
@@ -1,56 +1,114 @@
 import torch
-from itinero import CrimeClassifier
+from itinero_model import CrimeClassifier
 from news_data_fetcher import NewsAPI
-from torch.utils.data import DataLoader, Dataset
-from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import BertForSequenceClassification, BertTokenizer, AdamW, get_linear_schedule_with_warmup
 
-# Load pre-trained BERT model and tokenizer
-model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+#---------------------------------------------------------------------------------------------
+# 1) Preprocess and split the data into training, validation, and testing sets
+#---------------------------------------------------------------------------------------------
 
-# Establish API connection and gather articles
+# Initialize the fine-tuned Bert classifier
+crime_classifier = CrimeClassifier()
+
+# Establish API connection
 news_api = NewsAPI()
 
+# Gather crime-related articles and preprocess
 crime_articles = news_api.get_crime_articles(max_articles=500)
+processed_crime_data = crime_classifier.preprocess_articles(crime_articles, label=1)
+
+# Gather non-crime-related articles and preprocess
 non_crime_articles = news_api.get_non_crime_articles(max_articles=500)
+processed_non_crime_data = crime_classifier.preprocess_articles(non_crime_articles, label=0)
 
-# Label the available data
-labeled_data = [{"text": article_title, "label": 1} for article_title in crime_articles] + \
-               [{"text": article_title, "label": 0} for article_title in non_crime_articles]
+# Combine the datasets
+tokenized_data = processed_crime_data + processed_non_crime_data
 
-# Define class for trainer datasets
-class ArticleDataset(Dataset):
-    def __init__(self, data, tokenizer, max_length=128):
-        self.data = data
-        self.tokenizer = tokenizer
-        self.max_length = max_length
+# Define the split ratios: training (70%), validation (15%), and testing (15%)
+train_data, val_data, test_data = crime_classifier.split_data(tokenized_data, 0.7, 0.15, 0.15)
 
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, index):
-        item = self.data[index]
-        text = item["text"]
-        label = item["label"]
-        encoding = self.tokenizer(text, padding="max_length", truncation=True, max_length=self.max_length)
-        return {
-            "input_ids": torch.tensor(encoding["input_ids"], dtype=torch.long),
-            "attention_mask": torch.tensor(encoding["attention_mask"], dtype=torch.long),
-            "labels": torch.tensor(label, dtype=torch.long),
-        }
+#---------------------------------------------------------------------------------------------
+# 2) Create DataLoaders and TensorDatasets for training, validation, and testing
+#---------------------------------------------------------------------------------------------
 
-# Create a custom dataset
-custom_dataset = ArticleDataset(data=labeled_data, tokenizer=tokenizer)
-
-# Create a DataLoader
 batch_size = 32
-data_loader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=True)
+train_loader = crime_classifier.create_dataloader(train_data, batch_size=batch_size, shuffle=True)
+val_loader = crime_classifier.create_dataloader(val_data, batch_size=batch_size, shuffle=False)
+test_loader = crime_classifier.create_dataloader(test_data, batch_size=batch_size, shuffle=False)
 
-# Initialize CrimeClassifier
-vocab_size = len(tokenizer)
-crime_classifier = CrimeClassifier(vocab_size=vocab_size)
+#---------------------------------------------------------------------------------------------
+# 3) Implement training loop with validation
+#---------------------------------------------------------------------------------------------
 
-crime_classifier.train_model(data_loader)
+# Initialize the BERT model and optimizer
+model = crime_classifier.model
+optimizer = AdamW(model.parameters(), lr=2e-5, no_deprecation_warning=True)
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=len(train_loader))
 
-model_path = "crime_classifier_model.pth"
-crime_classifier.save_model(model_path)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+epochs = 3
+best_val_accuracy = 0.0
+best_model = None
+
+for epoch in range(epochs):
+    model.train()
+    for train_batch in train_loader:
+        input_ids, masks, labels = train_batch
+        input_ids, masks, labels = input_ids.to(device), masks.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(input_ids, attention_mask=masks, labels=labels)
+        loss = outputs.loss
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+    
+    model.eval()
+    val_total = 0
+    val_correct = 0
+    with torch.no_grad():
+        for val_batch in val_loader:
+            input_ids, masks, labels = val_batch
+            input_ids, masks, labels = input_ids.to(device), masks.to(device), labels.to(device)
+
+            outputs = model(input_ids, attention_mask=masks)
+            _, predicted = torch.max(outputs.logits, 1)
+            val_total += labels.size(0)
+            val_correct += (predicted == labels).sum().item()
+    
+    val_accuracy = 100 * val_correct / val_total
+
+    print(f"Epoch {epoch + 1} - Validation Accuracy: {val_accuracy:.2f}%")
+
+    if val_accuracy > best_val_accuracy:
+        best_val_accuracy = val_accuracy
+        best_model = model.state_dict()
+    
+if best_model:
+    torch.save(best_model, "./itinero/backend/app/best_model_state.pth")
+
+#---------------------------------------------------------------------------------------------
+# 4) Testing/deployment
+#---------------------------------------------------------------------------------------------
+
+# Use the best model available for testing or deployment
+
+model.load_state_dict(torch.load("./itinero/backend/app/best_model_state.pth"))
+model.eval()
+
+test_total = 0
+test_correct = 0
+with torch.no_grad():
+    for test_batch in test_loader:
+        input_ids, masks, labels = test_batch
+        input_ids, masks, labels = input_ids.to(device), masks.to(device), labels.to(device)
+
+        outputs = model(input_ids, attention_mask=masks)
+        _, predicted = torch.max(outputs.logits, 1)
+        test_total += labels.size(0)
+        test_correct += (predicted == labels).sum().item()
+
+test_accuracy = 100 * test_correct / test_total
+print(f"Test Accuracy: {test_accuracy:.2f}%")
