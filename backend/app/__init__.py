b381233aafe8f1581603b253fee5b0cc98092e2a
Alejandro Cores <91358116+alec0322@users.noreply.github.com>
2023-10-24 16:19:29 -0400

    Update __init__.py
    Established a proper connection between the NewsAPI and the CrimeClassifier model

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,31 +1,15 @@
+from itinero_model import CrimeClassifier
 from news_data_fetcher import NewsAPI
-from itinero import CrimeClassifier
 
-#Establish API connection
-api_key = "1f2e3aff9ef144a99d66c1dd6bf3a6b7"
-news_api = NewsAPI(api_key=api_key)
+# Establish API connection
+news_api = NewsAPI()
 
-#Wait for user to specify what city they want to travel to (Pseudocode)
-'''
+# Wait for user to specify what city they want to travel to
 city = input("Which city would you like to travel to?\n")
 city_articles = news_api.get_city_articles(city)
-'''
 
-#Create labeled dataset for the model
-training_data = {}
-training_data["crime_articles"] = news_api.get_crime_articles()
-training_data["non_crime_articles"] = news_api.get_non_crime_articles()
-
-print(training_data) #Prints the training_data to be given to the model
-
-#Utilize Itinero to classify any crime-related articles (Pseudocode)
-'''
+# Utilize Itinero to classify any crime-related articles
 crime_classifier = CrimeClassifier()
 
-crime_related_titles = []
-for title in city_articles:
-    if crime_classifier.classify_articles(title):
-        crime_related_titles.append(title)
-
-print(crime_related_titles)
-'''
+# The model returns a set of articles which could potentially be crime-related
+print(crime_classifier.classify_articles(city_articles))
